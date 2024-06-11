7e157ab41664cf09330ed0a27560435f4860b5bf
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-12 03:30:40 +0545

    Creating auth repository

---
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -0,0 +1,28 @@
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeAuthRepository {
+  Stream<AppUser?> authStateChanges() => Stream.value(null);
+  AppUser? get currentUser => null;
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    return;
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    return;
+  }
+
+  Future<void> signOut() async {
+    return;
+  }
+}
+
+final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
+  return FakeAuthRepository();
+});
+
+final authStateChangesProvider = StreamProvider<AppUser?>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return authRepository.authStateChanges();
+});
