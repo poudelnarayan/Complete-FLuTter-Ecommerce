8d6b65261c6c6b0a941e6f085f2209dca5e139f1
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-11 14:12:20 +0545

    working with futures and stream api

--- lib/src/features/products/data/fake_products_repository.dart
+++ lib/src/features/products/data/fake_products_repository.dart
@@ -4,11 +4,28 @@ import 'package:ecommerce_app/src/features/products/domain/product.dart';
 class FakeProductsRepository {
   FakeProductsRepository._();
   static FakeProductsRepository instance = FakeProductsRepository._();
+
+  final List<Product> _products = kTestProducts;
+
   List<Product> getProductList() {
-    return kTestProducts;
+    return _products;
   }
 
   Product? getProductById(String id) {
-    return kTestProducts.firstWhere((product) => product.id == id);
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchProductsList() {
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchProductsList() {
+    return Stream.value(_products);
+  }
+
+  Stream<Product?> watchProductById(String id) {
+    return watchProductsList().map((products) {
+      return products.firstWhere((product) => product.id == id);
+    });
   }
 }
