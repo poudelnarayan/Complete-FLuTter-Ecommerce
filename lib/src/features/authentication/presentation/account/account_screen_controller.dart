120eaab971c7fd20beec4909082dbf7f11df7515
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-12 23:40:30 +0545

    Creating first controller using statenotifier

---
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Controllers are used for : managing the state of the screen, handling user interactions , Mediate between the UI and the repository
+/// controllers are similar to View models in MVVM architecture , Blocs in BLoC architecture
+
+/// Controllers using StateNotifier(Riverpod) are similar to:
+/// ValueNotifier/ChangeNotifier in FlutterSDK
+/// Cubit in Bloc
+
+/// Async notifier is an alternative to StateNotifier that is designed to work with AsyncValue.
+
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+  /// StateNotifier always needs an initial value , this needs to be set with the constructor using super()
+  AccountScreenController(this.authRepository)
+      : super(const AsyncValue<void>.data(null));
+  final FakeAuthRepository authRepository;
+  Future<void> logout() async {
+    try {
+      // set the state to loading
+      state = const AsyncValue<void>.loading();
+      // signout (usingg auth repository)
+      await authRepository.signOut();
+      // set the state to data (if success)
+      state = const AsyncValue<void>.data(null);
+    } catch (e, st) {
+      // set the state to error (if error)
+      state = AsyncValue.error(e, st);
+    }
+  }
+}
+
+// create a provider for the controller
+final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
+    AccountScreenController, AsyncValue<void>>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AccountScreenController(authRepository);
+});
