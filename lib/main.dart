01bbe15dc04c1c33ef18a8917ba62af54d4018ad
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-15 22:57:20 +0545

    gorouter Redirects

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // ignore:depend_on_referenced_packages
-// import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // turn off the # in the URLs on the web
-  // usePathUrlStrategy();
+  usePathUrlStrategy();
   // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
