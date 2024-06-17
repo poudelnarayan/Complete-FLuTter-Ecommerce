f5840d988333262fe909fd342bf5987b4ab2538d
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-17 16:48:39 +0545

    Robot Testing

--- test/src/features/authentication/presentation/account/account_screen_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -3,27 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../auth_robot.dart';
+
 void main() {
   testWidgets('Cancel Logout', (tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
-          home: AccountScreen(),
-        ),
-      ),
-    );
-    final logoutButton = find.text('Logout');
-    expect(logoutButton, findsOneWidget);
-    // The test environment does not update the UI after we
-    // an interaction (e.g. tap on a button)
-    await tester.tap(logoutButton);
-    await tester.pump(); // this will trigger a new frame
-    final dialogTitle = find.text('Are you sure?');
-    expect(dialogTitle, findsOneWidget);
-    final cancelButton = find.text('Cancel');
-    expect(cancelButton, findsOneWidget);
-    await tester.tap(cancelButton);
-    await tester.pump();
-    expect(dialogTitle, findsNothing);
+    final r = AuthRobot(tester);
+    await r.pumpAccountScreen();
+    await r.tapLogoutButton();
+    r.expectLofoutDialogFound();
+    await r.tapCancelButton();
+    r.expectLogoutDialogNotFound();
   });
 }
