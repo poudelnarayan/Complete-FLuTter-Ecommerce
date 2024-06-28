60d335f6ce03c936fbf37cd3f85268294753c902
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-28 10:42:40 +0545

    Updating the CartService class to read dependencies using ref

--- lib/src/features/cart/application/cart_service.dart
+++ lib/src/features/cart/application/cart_service.dart
@@ -8,33 +8,36 @@ import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartService {
-  final FakeAuthRepository authRepository;
-  final LocalCartRepository localCartRepository;
-  final RemoteCartRepository remoteCartRepository;
+  final Ref ref;
 
-  CartService({
-    required this.authRepository,
-    required this.localCartRepository,
-    required this.remoteCartRepository,
-  });
+  CartService(this.ref);
+  // final FakeAuthRepository authRepository;
+  // final LocalCartRepository localCartRepository;
+  // final RemoteCartRepository remoteCartRepository;
+
+  // CartService({
+  //   required this.authRepository,
+  //   required this.localCartRepository,
+  //   required this.remoteCartRepository,
+  // });
 
   Future<Cart> _fetchCart() async {
-    final user = authRepository.currentUser;
+    final user = ref.read(authRepositoryProvider).currentUser;
     if (user != null) {
-      return remoteCartRepository.fetchCart(user.uid);
+      return ref.read(remoteCartRepositoryProvider).fetchCart(user.uid);
     } else {
-      return localCartRepository.fetchCart();
+      return ref.read(localCartRepositoryProvider).fetchCart();
     }
   }
 
   /// save the cart to the local or remote repository
   /// depending on the user auth state
   Future<void> _setCart(Cart cart) async {
-    final user = authRepository.currentUser;
+    final user = ref.read(authRepositoryProvider).currentUser;
     if (user != null) {
-      await remoteCartRepository.setCart(user.uid, cart);
+      await ref.read(remoteCartRepositoryProvider).setCart(user.uid, cart);
     } else {
-      await localCartRepository.setCart(cart);
+      await ref.read(localCartRepositoryProvider).setCart(cart);
     }
   }
 
@@ -56,14 +59,9 @@ class CartService {
     final cart = await _fetchCart();
     final updated = cart.removeItemById(productId);
     await _setCart(updated);
-  
   }
 }
 
 final cartServiceProvider = Provider<CartService>((ref) {
-  return CartService(
-    authRepository: ref.watch(authRepositoryProvider),
-    localCartRepository: ref.watch(localCartRepositoryProvider),
-    remoteCartRepository: ref.watch(remoteCartRepositoryProvider),
-  );
+  return CartService(ref);
 });
