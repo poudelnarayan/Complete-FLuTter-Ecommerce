2034c1441217c9bfbb4db82a0eb9e785945c36c0
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-29 10:56:45 +0545

    Implementing AddToCartController

---
+++ lib/src/features/cart/presentation/add_to_cart/add_to_cart_controller.dart
@@ -0,0 +1,37 @@
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../domain/item.dart';
+
+class AddToCartController extends StateNotifier<AsyncValue<int>> {
+  AddToCartController({required this.cartService}) : super(const AsyncData(1));
+  final CartService cartService;
+
+  void updateQuantity(int quantity) {
+    state = AsyncData(quantity);
+  }
+
+  Future<void> additem(ProductID productId) async {
+    final item = Item(productId: productId, quantity: state.value!);
+    // If we use only  AsyncLoading , then we no longer have a way to keep track of the state because
+    // AsyncLoading doesnt carry any information about the previous  state.
+    // state = const AsyncLoading();
+    // so
+    state = const AsyncLoading<int>().copyWithPrevious(state);
+    final value = await AsyncValue.guard(() => cartService.addItem(item));
+    if (value.hasError) {
+      state = AsyncError(value.error!, value.stackTrace!);
+    } else {
+      state = const AsyncData(1);
+    }
+  }
+}
+
+// TODO: shoiuld this use autoDispose?
+final addtoCartControllerProvider =
+    StateNotifierProvider<AddToCartController, AsyncValue<int>>((ref) {
+  return AddToCartController(
+    cartService: ref.watch(cartServiceProvider),
+  );
+});
--- test/src/features/cart/application/cart_service_test.dart
+++ test/src/features/cart/application/cart_service_test.dart
@@ -1,13 +1,20 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../../mocks.dart';
 
 void main() {
+  setUpAll(() {
+    registerFallbackValue(const Cart());
+  });
   late MockAuthRepository authRepository;
   late MockRemoteCarRepository remoteCartRepository;
   late MockLocalCartRepository localCartRepository;
@@ -32,9 +39,39 @@ void main() {
 
   group('setItem', () {
     test('null user , writes item to local cart', () async {
+      //setup
+      const expectedCart = Cart({'123': 1});
+      when(() => authRepository.currentUser).thenReturn(null);
+      when(localCartRepository.fetchCart)
+          .thenAnswer((_) => Future.value(const Cart()));
+      when(() => localCartRepository.setCart(expectedCart))
+          .thenAnswer((_) => Future.value());
       final cartService = makeCartService();
+      //run
+      await cartService.setItem(const Item(productId: '123', quantity: 1));
+
+      //verify
+      verify(() => localCartRepository.setCart(expectedCart)).called(1);
+      verifyNever(() => remoteCartRepository.setCart(any(), any()));
     });
 
-    test('non-null user , writes item to remote cart', () async {});
+    test('non-null user , writes item to remote cart', () async {
+      //setup
+      const testUser = AppUser(uid: "abc");
+      const expectedCart = Cart({'123': 1});
+      when(() => authRepository.currentUser).thenReturn(testUser);
+      when(() => remoteCartRepository.fetchCart(testUser.uid))
+          .thenAnswer((_) => Future.value(const Cart()));
+      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
+          .thenAnswer((_) => Future.value());
+      final cartService = makeCartService();
+      //run
+      await cartService.setItem(const Item(productId: '123', quantity: 1));
+
+      //verify
+      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
+          .called(1);
+      verifyNever(() => localCartRepository.setCart(any()));
+    });
   });
 }
