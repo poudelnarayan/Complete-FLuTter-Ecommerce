34afb8f9c88f69c51f43564eabb39706923e1fbd
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-29 17:56:28 +0545

    Implementinf ShoppingCartItem controller

---
+++ lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen_controller.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ShoppingCartScreenController extends StateNotifier<AsyncValue<void>> {
+  final CartService cartService;
+
+  ShoppingCartScreenController({required this.cartService})
+      : super(const AsyncData(null));
+
+  Future<void> updateItemQuantity(ProductID productId, int quantity) async {
+    state = const AsyncLoading();
+    final updatedItem = Item(productId: productId, quantity: quantity);
+    state = await AsyncValue.guard(() => cartService.setItem(updatedItem));
+  }
+
+  Future<void> removeItemById(ProductID productId) async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() => cartService.removeItemById(productId));
+  }
+}
+
+final shooppinCartScreenControllerProvider =
+    StateNotifierProvider<ShoppingCartScreenController, AsyncValue<void>>(
+        (ref) {
+  return ShoppingCartScreenController(
+    cartService: ref.watch(cartServiceProvider),
+  );
+});
