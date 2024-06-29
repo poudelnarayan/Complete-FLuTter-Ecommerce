807f56f43310c0560a9e1b8ee69bc44833109d83
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-29 20:49:18 +0545

    Implementing the logic inside the CartSyncService

--- lib/src/features/cart/application/cart_sync_service.dart
+++ lib/src/features/cart/application/cart_sync_service.dart
@@ -1,5 +1,10 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/cart/domain/mutable_cart.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartSyncService {
@@ -16,13 +21,28 @@ class CartSyncService {
       final user = next.value;
       // previously not logged in and now logged in
       if (previousUser == null && user != null) {
-        // user logged in
-        // sync local cart with remote cart
-      } else if (previousUser != null && user == null) {
-        // user logged out
-        // sync remote cart with local cart
+        _moveItemsToRemoteCart(user.uid);
       }
     });
+  }
+
+  /// moves all items from the local to the remote cart taking into the account the available quantities
+  Future<void> _moveItemsToRemoteCart(String uid) async {
+    // get the local cart data
+
+    final localCartRepository = ref.read(localCartRepositoryProvider);
+    final localCart = await localCartRepository.fetchCart();
+    if (localCart.items.isNotEmpty) {
+      // get the remote cart data
+      final remoteCartRepository = ref.read(remoteCartRepositoryProvider);
+      final remoteCart = await remoteCartRepository.fetchCart(uid);
+      final localItemsToAdd = localCart.toItemsList();
+      // add all the local items to the remote cart
+      final updatedRemoteCart = remoteCart.addItems(localItemsToAdd);
+      // write the updated remote cart data to the repository
+      await remoteCartRepository.setCart(uid, updatedRemoteCart);
+      // Remove all items from the local cart
+    }
   }
 }
 
